8e69c7d39f136fdea7e41a4ba3e17b634c8a9705
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-21 00:11:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,12 +2,10 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
-def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
+def position_taken?(board,index)
+  if board[index]="X" || board[index] = "O"
     return true
   else
     return false
   end
-
 end
